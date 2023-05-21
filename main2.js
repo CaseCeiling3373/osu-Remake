2caef56ef519e3ea682177d4d67f8dd5852ebc3d
CaseCeiling3373 <100453141+CaseCeiling3373@users.noreply.github.com>
2023-05-20 21:57:04 -0400

    Add files via upload

---
+++ main2.js
@@ -0,0 +1,41 @@
+var mapList = document.getElementById('mapList');
+var previewImg = document.getElementById('previewImg');
+
+var mapInfoList = [
+    {
+        folderName : "dreamSpace",
+        title : "Dream Space",
+        artist : "DVRST",
+        starRate: 3.11
+    },{
+        folderName : "murderInMyMind",
+        title : "Murder In My Mind",
+        artist : "Kordhell",
+        starRate: 3.28
+    }
+]
+
+function createMap(idx){
+    var map = document.createElement('div');
+    map.classList = 'map';
+
+    var h3 = document.createElement('h3');
+    var h4 = document.createElement('h4');
+    var h5 = document.createElement('h5');
+
+    map.appendChild(h4);
+    map.appendChild(h5);
+    map.appendChild(h3);
+
+    h3.innerHTML = mapInfoList[idx].starRate;
+    h4.innerHTML = mapInfoList[idx].title;
+    h5.innerHTML = mapInfoList[idx].artist;
+
+    mapList.appendChild(map);
+    map.addEventListener('mouseover', () => {
+        previewImg.background = 'url(' + folderName + '/background.jpg)';
+    });
+}
+
+createMap(0);
+createMap(1);
